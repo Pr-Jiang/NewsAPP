6070313d295926b9f3d7b04ce4f37398d735b0cb
Mr.fish <jiangruifly@gmail.com>
2016-11-09 16:16:31 +0800

    添加闪屏界面

---
+++ app/src/main/java/com/example/jiangrui/newsapp/Activity/SplashActivity.java
@@ -0,0 +1,64 @@
+package com.example.jiangrui.newsapp.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.example.jiangrui.newsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by jiangrui on 2016/11/9.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.splash_image)
+    ImageView splashImage;
+
+    private ScaleAnimation scaleAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.TYPE_STATUS_BAR,WindowManager.LayoutParams.TYPE_STATUS_BAR);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        splashImage.setImageResource(R.drawable.splash);
+
+        scaleAnimation = new ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setDuration(3000);
+
+        splashImage.startAnimation(scaleAnimation);
+
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
